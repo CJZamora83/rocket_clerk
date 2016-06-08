7843ec5d208c02cb1993acebb3208f033902439c
Christian Zamora <christian.j.zamora@gmail.com>
2016-06-08 14:53:16 -0700

    logging out

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
 
     root "welcome#index"
 
-    resources :user, only: [:post]
+    resources :users
     resources :sessions, only: [:new, :create, :destroy]
+
 
     get '/login', to: 'sessions#new'
     post '/user', to: 'users#create'
