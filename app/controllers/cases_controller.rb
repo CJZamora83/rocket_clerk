f2f4842bc4914ceb8b849cf5f7d715ab80126288
Christian Zamora <christian.j.zamora@gmail.com>
2016-06-08 14:58:42 -0700

    new case page

---
+++ app/controllers/cases_controller.rb
@@ -0,0 +1,6 @@
+class CasesController < ApplicationController
+
+  def new
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,12 @@ Rails.application.routes.draw do
 
     resources :users
     resources :sessions, only: [:new, :create, :destroy]
+    resources :cases, only: [:new]
 
 
     get '/login', to: 'sessions#new'
     post '/user', to: 'users#create'
     delete '/logout', to: 'sessions#destroy'
+
 
 end
