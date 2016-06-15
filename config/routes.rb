5185ad787aaf4b92fd202b665fad7ebdbabdeeca
Christian Zamora <christian.j.zamora@gmail.com>
2016-06-14 20:47:49 -0700

    homepage statement update

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
 
-   root "welcome#index"
-     resources :users
-     resources :sessions, only: [:new, :create, :destroy]
-     resources :cases do
-     resources :entries
-     end
+  root "welcome#index"
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :cases do
+    resources :entries
+  end
 
   get '/login', to: 'sessions#new'
   delete '/logout', to: 'sessions#destroy'
   delete '/entries/:id', to: 'entries#destroy', as: 'entry'
 
+  get '/about', to: 'pages#about', as: 'about'
+
 end
+
+
